f4edc167739e7f8fa9ed8a613e7946be84205f4e
le-tfr <132360159+le-tfr@users.noreply.github.com>
2024-04-11 11:22:36 +0200

    add data files

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-import requests
 from io import StringIO
 
+# Import and concatenate MOD1
+mod1_parts = []
+for i in range(1, 9):
+    temp = pd.read_csv(f"data/Libelium New/part{i}/mod1.txt", sep="\t", header=None, names=("Time", "RH", "Temperature", "TGS4161", "MICS2714", "TGS2442", "MICS5524", "TGS2602", "TGS2620"))
+    mod1_parts.append(temp)
+
+mod1 = pd.concat(mod1_parts)
+
+# Import and concatenate MOD2
+mod2_parts = []
+for i in range(1, 9):
+    temp = pd.read_csv(f"data/Libelium New/part{i}/mod2.txt", sep="\t", header=None, names=("Time", "RH", "Temperature", "TGS4161", "MICS2714", "TGS2442", "MICS5524", "TGS2602", "TGS2620"))
+    mod2_parts.append(temp)
+
+mod2 = pd.concat(mod2_parts)
+
+# Import and concatenate PODs
+pod_files = {
+    "POD 200085": ["data/PODs/14_nov-22_nov-Pods/POD 200085.csv", "data/PODs/23_nov-12_dec-Pods/POD 200085.csv", "data/PODs/fevrier_mars_2023_pods/POD 200085.csv"],
+    "POD 200086": ["data/PODs/14_nov-22_nov-Pods/POD 200086.csv", "data/PODs/23_nov-12_dec-Pods/POD 200086.csv", "data/PODs/fevrier_mars_2023_pods/POD 200086.csv"],
+    "POD 200088": ["data/PODs/14_nov-22_nov-Pods/POD 200088.csv", "data/PODs/23_nov-12_dec-Pods/POD 200088.csv", "data/PODs/fevrier_mars_2023_pods/POD 200088.csv"]
+}
+
+pods = {}
+
+for key, value in pod_files.items():
+    pod_parts = []
+    for file_path in value:
+        temp = pd.read_csv(file_path, sep=";", skiprows=(1, 2, 3, 4))
+        pod_parts.append(temp)
+    pods[key] = pd.concat(pod_parts)
+
+# PICO, Piano THICK and Piano THIN are similar to the POD modules, use the same code but provide appropriate file paths
