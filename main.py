581d954acedad8389a0d49cb161d14c66389c19d
thmsczn <114753198+thmsczn@users.noreply.github.com>
2024-05-05 17:29:21 +0200

    mise en forme

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+"""main.py
+
+## IMPORTS
+"""
+
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-from io import StringIO
 import os
 from scipy.interpolate import interp1d
 import numpy as np
@@ -14,13 +17,17 @@ import torch.optim as optim
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
 
+"""
+## SEANCE 1
 
-print(colored("--------------> Séance 1 <--------------", "blue"))
+"""
+
+print(colored("\n--------------> Séance 1 <--------------", "blue"))
 
 if not os.path.exists("database"):
     os.makedirs("database")
 
-# Importer et concaténer MOD1
+# Importation et concaténation de MOD1
 mod1_parts = []
 for i in range(1, 9):
     temp = pd.read_csv(f"data/Libelium New/part{i}/mod1.txt", sep="\t", header=None, names=("Time", "RH", "Temperature", "TGS4161", "MICS2714", "TGS2442", "MICS5524", "TGS2602", "TGS2620"))
@@ -33,7 +40,7 @@ print(mod1.head())
 print("Colonnes MOD1:")
 print(mod1.columns)
 
-# Importer et concaténer MOD2
+# Importation et concaténation de MOD2
 mod2_parts = []
 for i in range(1, 9):
     temp = pd.read_csv(f"data/Libelium New/part{i}/mod2.txt", sep="\t", header=None, names=("Time", "RH", "Temperature", "TGS4161", "MICS2714", "TGS2442", "MICS5524", "TGS2602", "TGS2620"))
@@ -46,7 +53,7 @@ print(mod2.head())
 print("Colonnes MOD2:")
 print(mod2.columns)
 
-# Importer et concaténer PODs
+# Importation et concaténation de PODs
 pod_files = {
     "POD 200085": ["data/PODs/14_nov-22_nov-Pods/POD 200085.csv", "data/PODs/23_nov-12_dec-Pods/POD 200085.csv", "data/PODs/fevrier_mars_2023_pods/POD 200085.csv"],
     "POD 200086": ["data/PODs/14_nov-22_nov-Pods/POD 200086.csv", "data/PODs/23_nov-12_dec-Pods/POD 200086.csv", "data/PODs/fevrier_mars_2023_pods/POD 200086.csv"],
@@ -96,54 +103,59 @@ print("Enregistrement du fichier database.csv...")
 merged_data.to_csv("database/database.csv", index=False, sep=';')
 print("Fichier database.csv enregistré.")
 
-print(colored("--------------> Séance 2 <--------------", "blue"))
+"""
+## SEANCE 2
+
+"""
+
+print(colored("\n--------------> Séance 2 <--------------", "blue"))
 
 # Vérification des colonnes dans database.csv
 print("Vérification des colonnes de database.csv:")
 saved_base = pd.read_csv("database/database.csv", sep=';')
 print(saved_base.columns)
 
-# Tâche 3 : Charger la base de données
+# Chargement de la base de données
 base = pd.read_csv('database/database.csv', sep=';')
 
-# Convertir la colonne 'Time' au format datetime avec la timezone
+# Convertion de la colonne 'Time' au format datetime avec la timezone
 base['Time'] = pd.to_datetime(base['Time'], dayfirst=True).dt.tz_localize('UTC').dt.tz_convert('UTC+01:00')
 
 print(base.dtypes)
 
 activities = pd.read_excel("data/activites.xlsx", sheet_name="Done so far", usecols=["activity", "Started", "Ended"])
 
-# Supprimer les valeurs NaN
+# Suppression des valeurs NaN
 activities = activities.dropna()
 
-# Convertir les colonnes Started et Ended en format datetime avec le bon fuseau horaire
+# Convertion des colonnes Started et Ended en format datetime avec le bon fuseau horaire
 activities["Started"] = pd.to_datetime(activities["Started"]).dt.tz_localize('UTC').dt.tz_convert('UTC+01:00')
 activities["Ended"] = pd.to_datetime(activities["Ended"]).dt.tz_localize('UTC').dt.tz_convert('UTC+01:00')
 
-# Vérifier les types de données
+# Vérification des types de données
 print(activities.dtypes)
 
-# Vérifier les dimensions du DataFrame
+# Vérification des dimensions du DataFrame
 print(activities.shape)
 
-# Initialiser les variables pour stocker les instances d'activités et la longueur totale
+# Initialisation des variables pour stocker les instances d'activités et la longueur totale
 activity_instances = {}
 activity_lengths = {}
 
-# Initialiser les variables pour stocker les instances d'activités et la longueur totale
+# Initialisation des variables pour stocker les instances d'activités et la longueur totale
 activity_instances = {}
 activity_lengths = {}
 
-# Itérer à travers toutes les activités dans le calendrier
+# Itération à travers toutes les activités dans le calendrier
 for index, row in activities.iterrows():
     start = row['Started'].tz_convert('UTC+01:00')
     end = row['Ended'].tz_convert('UTC+01:00')
     act = row['activity']
 
-    # Extraire les données de l'activité actuelle
+    # Extraction des données de l'activité actuelle
     new_activity_data = base[(base['Time'] >= start) & (base['Time'] <= end)].reset_index(drop=True).sort_values(by='Time').drop(columns='Time')
 
-    # Ajouter l'activité actuelle à la liste des instances pour cette activité
+    # Ajout de l'activité actuelle à la liste des instances pour cette activité
     if act not in activity_instances:
         activity_instances[act] = []
         activity_lengths[act] = 0
@@ -152,21 +164,18 @@ for index, row in activities.iterrows():
     activity_lengths[act] += len(new_activity_data)
 
 def normalize_instance(instance, common_length):
-    if len(instance) == 0: # Vérifiez si l'instance est vide
+    if len(instance) == 0:
         return np.empty((0, common_length))
 
     original_length = len(instance)
     x = np.linspace(0, original_length - 1, num=common_length, endpoint=True)
     instance_array = np.array(instance.to_numpy())
-    f = interp1d(np.arange(original_length), instance_array, kind='linear', axis=0, bounds_error=False, fill_value="extrapolate")  # Ajoutez les arguments bounds_error et fill_value
+    f = interp1d(np.arange(original_length), instance_array, kind='linear', axis=0, bounds_error=False, fill_value="extrapolate")
     normalized_instance = f(x)
     return normalized_instance
 
-import numpy as np
-
 def averageSignature(signature_values, common_length):
     normalized_instances = [normalize_instance(instance, common_length) for instance in signature_values]
-    # Filtrer les instances vides ou contenant des NaN pour éviter les erreurs lors du calcul de la moyenne
     normalized_instances = [instance for instance in normalized_instances if len(instance) > 0 and not np.any(np.isnan(instance))]
     average_signature = np.mean(normalized_instances, axis=0)
     return average_signature
@@ -189,16 +198,16 @@ def plot_signatures(avg_activity_signatures, activity_labels):
 
     plt.savefig('activity_signatures.png', dpi=300)
 
-    #plt.show()
+    plt.show()
 
-# Créer une liste de toutes les instances d'activité et une liste des longueurs moyennes
+# Création d'une liste de toutes les instances d'activité et une liste des longueurs moyennes
 all_instances = [activity_instances[k] for k in sorted(activity_instances)]
 all_lengths = [activity_lengths[k] // len(activity_instances[k]) for k in sorted(activity_lengths)]
 
-# Calculer les signatures moyennes pour toutes les activités
+# Calcule des signatures moyennes pour toutes les activités
 avg_activity_signatures = averageSignatures(all_instances, all_lengths)
 
-# Imprimer les signatures moyennes pour chaque activité
+# Impression les signatures moyennes pour chaque activité
 activity_labels = sorted(activity_instances)
 for i in range(len(activity_labels)):
     print(f"Signature moyenne pour {activity_labels[i]} :")
@@ -207,7 +216,12 @@ for i in range(len(activity_labels)):
 
 plot_signatures(avg_activity_signatures, activity_labels)
 
-print(colored("--------------> Séance 3 <--------------", "blue"))
+"""
+## SEANCE 3
+
+"""
+
+print(colored("\n--------------> Séance 3 <--------------", "blue"))
 
 all_activities = []
 
@@ -222,7 +236,7 @@ for idact, row in activities.iterrows():
     activity_data['Label'] = idact
     all_activities.append(activity_data)
 
-# Concaténer tous les sous-ensembles d'activités en un seul dataframe
+# Concaténation de tous les sous-ensembles d'activités en un seul dataframe
 activity_df = pd.concat(all_activities, axis=0, ignore_index=True)
 
 activity_df.to_csv("activities_dataset.csv", index=False, sep=';')
@@ -230,7 +244,6 @@ activity_df.to_csv("activities_dataset.csv", index=False, sep=';')
 activity_label_mapping = {old_label: new_label for new_label, old_label in enumerate(sorted(activity_instances))}
 print("Dictionnaire d'activités :")
 print(activity_label_mapping)
-
 
 class CustomDataset(Dataset):
     def __init__(self, csv_file):
@@ -264,8 +277,8 @@ class SimpleNet(nn.Module):
         return out
     
 # Dimensions d'entrée et de sortie
-input_size = len(activity_df.columns) - 2  # Nombre de colonnes moins les colonnes Activity et Label
-num_classes = 10  # Nombre d'activités
+input_size = len(activity_df.columns) - 2 
+num_classes = 10
 
 # Création du modèle, la fonction de coût et l'optimiseur
 model = SimpleNet(input_size, num_classes)
@@ -287,7 +300,12 @@ for epoch in range(num_epochs):
 
 print("Entraînement terminé")
 
-print(colored("--------------> Séance 4 <--------------", "blue"))
+"""
+## SEANCE 4
+
+"""
+
+print(colored("\n--------------> Séance 4 <--------------", "blue"))
 
 def create_sequences(dataset, seq_length):
     seqs = []
@@ -295,31 +313,31 @@ def create_sequences(dataset, seq_length):
     for i in range(len(dataset) - seq_length):
         seq = dataset.iloc[i:i+seq_length, :-2].values[np.newaxis, :, :]
         seqs.append(seq)
-        label = dataset.iloc[i+seq_length-1, -1]  # Obtenez directement l'étiquette scalaire
+        label = dataset.iloc[i+seq_length-1, -1]
         labels.append(label)
     if seqs and labels:
         return np.concatenate(seqs, axis=0), labels
     else:
         return None, None
 
-# Choisissez la longueur des séquences pour créer les données d'entrée du LSTM
+# Détermination de la longueur des séquences pour créer les données d'entrée du LSTM
 seq_length = 20
 
-# Créez des séquences pour chaque activité
+# Création des séquences pour chaque activité
 activity_sequences = {}
 for activity, instances in activity_instances.items():
     sequences_and_labels = [create_sequences(instance, seq_length) for instance in instances if not instance.empty]
-    if sequences_and_labels:  # Vérifie si la liste n'est pas vide
+    if sequences_and_labels:
         activity_sequences[activity] = sequences_and_labels
 
-# Concaténez toutes les séquences et les étiquettes pour les différentes activités
+# Concaténation de toutes les séquences et les étiquettes pour les différentes activités
 X_all = np.concatenate([seq for activity, seqs_and_labels in activity_sequences.items() for seq, _ in seqs_and_labels if seq is not None], axis=0)
 y_all = np.hstack([label for activity, seqs_and_labels in activity_sequences.items() for _, label in seqs_and_labels if label is not None])
 
-# Divisez les données en ensembles d'entraînement et de test
+# Division des données en ensembles d'entraînement et de test
 X_train, X_test, y_train, y_test = train_test_split(X_all, y_all, test_size=0.2, random_state=42)
 
-# Convertissez les données en tenseurs PyTorch
+# Convertion des données en tenseurs PyTorch
 X_train = torch.tensor(X_train, dtype=torch.float32)
 y_train = torch.tensor(y_train, dtype=torch.long)
 X_test = torch.tensor(X_test, dtype=torch.float32)
@@ -347,18 +365,18 @@ class LSTMClassifier(nn.Module):
         out = self.fc(out[:, -1, :])
         return out
 
-input_size = X_all.shape[2]  # Nombre de caractéristiques
+input_size = X_all.shape[2]
 hidden_size = 64
 num_layers = 2
-num_classes = len(activity_label_mapping)  # Nombre total d'activités uniques
+num_classes = len(activity_label_mapping)
 
 model = LSTMClassifier(input_size, hidden_size, num_layers, num_classes)
 
-# Définissez la fonction de coût et l'optimiseur
+# Définition de la fonction de coût et l'optimiseur
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-# Entraînez le modèle
+# Entraînenement du modèle
 num_epochs = 50
 for epoch in range(num_epochs):
     running_loss = 0.0
@@ -371,7 +389,7 @@ for epoch in range(num_epochs):
         running_loss += loss.item()
     print(f"Époque: {epoch + 1}, Perte: {running_loss / len(train_dataloader)}")
 
-# Testez le modèle
+# Test du modèle
 model.eval()
 y_true = []
 y_pred = []
